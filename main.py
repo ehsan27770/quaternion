8ebbf13edb724e5b711de38c7d6401208ee69f66
ehsan27770 <ehsan27770@gmail.com>
2024-07-19 15:37:23 +0200

    optimized using numpy functionality

--- main.py
+++ main.py
@@ -1,31 +1,165 @@
 import numpy as np
-from utils import Quaternion, Vector, Vector3D, Quaternion4D, Rotation_Euler, create_3d_curve, create_3d_surface
+from utils import Vector3D, Quaternion4D, Rotation_Euler, create_3d_curve, create_3d_surface
 # import for 3d plotting
 import matplotlib.pyplot as plt
+import tkinter as tk
+from tkinter import ttk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg 
+
+class App:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("3D Rotation")
+
+        self.frame1 = ttk.Frame(self.root)
+        self.frame1.grid(row=0, column=0)
+        self.frame2 = ttk.Frame(self.root)
+        self.frame2.grid(row=0, column=1)
+
+        self.u_x = tk.DoubleVar(value=1)
+        self.u_y = tk.DoubleVar(value=0)
+        self.u_z = tk.DoubleVar(value=0)
+        self.theta = tk.DoubleVar(value=np.pi/3)
+
+        self.label_u_x = ttk.Label(self.root, text='u_x')
+        self.label_u_x.grid(row=1, column=0,columnspan=1)
+        self.slider_u_x = ttk.Scale(self.root, from_=-1, to=1, variable=self.u_x, orient='horizontal', command=self.update)
+        self.slider_u_x.grid(row=1, column=1,columnspan=5,sticky='we')
+
+        self.label_u_y = ttk.Label(self.root, text='u_y')
+        self.label_u_y.grid(row=2, column=0,columnspan=1)
+        self.slider_u_y = ttk.Scale(self.root, from_=-1, to=1, variable=self.u_y, orient='horizontal', command=self.update)
+        self.slider_u_y.grid(row=2, column=1,columnspan=5,sticky='we')
+
+        self.label_u_z = ttk.Label(self.root, text='u_z')
+        self.label_u_z.grid(row=3, column=0,columnspan=1)
+        self.slider_u_z = ttk.Scale(self.root, from_=-1, to=1, variable=self.u_z, orient='horizontal', command=self.update)
+        self.slider_u_z.grid(row=3, column=1,columnspan=5,sticky='we')
+
+        self.label_theta = ttk.Label(self.root, text='theta')
+        self.label_theta.grid(row=4, column=0,columnspan=1)
+        self.slider_theta = ttk.Scale(self.root, from_=-2*np.pi, to=2*np.pi, variable=self.theta, orient='horizontal', command=self.update)
+        self.slider_theta.grid(row=4, column=1,columnspan=5,sticky='we')
+
+
+        self.fig1, self.ax1= plt.subplots(figsize=(10, 10), subplot_kw={'projection': '3d'})
+        self.canvas1 = FigureCanvasTkAgg(self.fig1, master=self.frame1)
+        self.canvas1.get_tk_widget().grid(row=0, column=0,columnspan=6)
+
+        self.fig2, self.ax2= plt.subplots(figsize=(10, 10), subplot_kw={'projection': '3d'})
+        self.canvas2 = FigureCanvasTkAgg(self.fig2, master=self.frame2)
+        self.canvas2.get_tk_widget().grid(row=0, column=0,columnspan=6)
+
+        self.x, self.y, self.z = create_3d_curve()
+        self.X, self.Y, self.Z = create_3d_surface()
+
+        self.update()
+
+    def update(self, event=None):
+        self.update_plot()
+
+    def update_plot(self, event=None):
+
+
+        theta = self.theta.get()
+        s = np.ones((1,1))
+        u = np.array([self.u_x.get(), self.u_y.get(), self.u_z.get()])
+        u = np.expand_dims(u,axis=0)
+        u = u / np.linalg.norm(u)
+        q = Quaternion4D(np.concatenate([s* np.cos(theta/2),u * np.sin(theta/2)],axis=-1))
+        q._normalize()
+
+        v = Vector3D(np.stack((self.x, self.y, self.z),axis=-1))
+        V = Vector3D(np.stack((self.X, self.Y, self.Z),axis=-1))
+        cartesian_axis = 3*Vector3D(np.array([[1,0,0],[0,1,0],[0,0,1]]))
+        cartesian_axis_rotated = q.rotate(cartesian_axis)
+
+        v_rotated = q.rotate(v)
+        x_rotated,y_rotated,z_rotated = v_rotated[...,0],v_rotated[...,1],v_rotated[...,2]
+
+        V_rotated = q.rotate(V)
+        X_rotated,Y_rotated,Z_rotated = V_rotated[...,0],V_rotated[...,1],V_rotated[...,2]
+
+        self.ax1.clear()
+        self.ax1.plot(self.x, self.y, self.z, label='original', color='blue')
+        self.ax1.plot(x_rotated, y_rotated, z_rotated, label='rotated', color='green',alpha=0.5)
+        #plot vector u from origin
+        self.ax1.plot([0,u[0,0]],[0,u[0,1]],[0,u[0,2]],label='u',color='red')
+        #plot cartesian axis original
+        self.ax1.plot([0,cartesian_axis[0,0]],[0,cartesian_axis[0,1]],[0,cartesian_axis[0,2]],label='original axis',color='black',linestyle='--',alpha=0.5)
+        self.ax1.plot([0,cartesian_axis[1,0]],[0,cartesian_axis[1,1]],[0,cartesian_axis[1,2]],color='black',linestyle='--',alpha=0.5)
+        self.ax1.plot([0,cartesian_axis[2,0]],[0,cartesian_axis[2,1]],[0,cartesian_axis[2,2]],color='black',linestyle='--',alpha=0.5)
+        #plot cartesian axis rotated
+        self.ax1.plot([0,cartesian_axis_rotated[0,0]],[0,cartesian_axis_rotated[0,1]],[0,cartesian_axis_rotated[0,2]],label='rotated axis',color='red',linestyle='--',alpha=0.5)
+        self.ax1.plot([0,cartesian_axis_rotated[1,0]],[0,cartesian_axis_rotated[1,1]],[0,cartesian_axis_rotated[1,2]],color='red',linestyle='--',alpha=0.5)
+        self.ax1.plot([0,cartesian_axis_rotated[2,0]],[0,cartesian_axis_rotated[2,1]],[0,cartesian_axis_rotated[2,2]],color='red',linestyle='--',alpha=0.5)
+
+
+        self.ax1.set_xlim(-5, 5)
+        self.ax1.set_ylim(-5, 5)
+        self.ax1.set_zlim(-5, 5)
+        self.ax1.set_xlabel('X')
+        self.ax1.set_ylabel('Y')
+        self.ax1.set_zlabel('Z')
+        self.ax1.legend()
+        self.canvas1.draw()
+
+        self.ax2.clear()
+        self.ax2.plot_surface(self.X, self.Y, self.Z, cmap='viridis', alpha=0.2)
+        self.ax2.plot_surface(X_rotated, Y_rotated, Z_rotated, cmap='winter', alpha=0.4)
+        #plot vector u from origin
+        self.ax2.plot([0,u[0,0]],[0,u[0,1]],[0,u[0,2]],label='u',color='red')
+        #plot cartesian axis original
+        self.ax2.plot([0,cartesian_axis[0,0]],[0,cartesian_axis[0,1]],[0,cartesian_axis[0,2]],label='original axis',color='black',linestyle='--',alpha=0.5)
+        self.ax2.plot([0,cartesian_axis[1,0]],[0,cartesian_axis[1,1]],[0,cartesian_axis[1,2]],color='black',linestyle='--',alpha=0.5)
+        self.ax2.plot([0,cartesian_axis[2,0]],[0,cartesian_axis[2,1]],[0,cartesian_axis[2,2]],color='black',linestyle='--',alpha=0.5)
+        #plot cartesian axis rotated
+        self.ax2.plot([0,cartesian_axis_rotated[0,0]],[0,cartesian_axis_rotated[0,1]],[0,cartesian_axis_rotated[0,2]],label='rotated axis',color='red',linestyle='--',alpha=0.5)
+        self.ax2.plot([0,cartesian_axis_rotated[1,0]],[0,cartesian_axis_rotated[1,1]],[0,cartesian_axis_rotated[1,2]],color='red',linestyle='--',alpha=0.5)
+        self.ax2.plot([0,cartesian_axis_rotated[2,0]],[0,cartesian_axis_rotated[2,1]],[0,cartesian_axis_rotated[2,2]],color='red',linestyle='--',alpha=0.5)
+
+        self.ax2.set_xlim(-2, 2)
+        self.ax2.set_ylim(-2, 2)
+        self.ax2.set_zlim(-2, 2)
+        self.ax2.set_xlabel('X')
+        self.ax2.set_ylabel('Y')
+        self.ax2.set_zlabel('Z')
+        self.ax2.legend()
+        self.canvas2.draw()
+
 
 if __name__ == '__main__':
 
+    root = tk.Tk()
+    app = App(root)
+    root.mainloop() 
+
+    '''
+
     x,y,z = create_3d_curve()
-    x1,y1,z1 = np.zeros_like(x),np.zeros_like(y),np.zeros_like(z)
-    x2,y2,z2 = np.zeros_like(x),np.zeros_like(y),np.zeros_like(z)
 
     theta = np.pi/3
-    u = Vector(-3, 1, -2)
-    q = Quaternion.from_scaler_vector(np.cos(theta/2),np.sin(theta/2)*u)
+    s = np.ones((1,1)) * np.cos(theta/2)
+    u = np.array([1, 0, 0])
+    u = np.expand_dims(u,axis=0)
+    u = u / np.linalg.norm(u) * np.sin(theta/2)
+    q = Quaternion4D(np.concatenate([s,u],axis=-1))
     q._normalize()
 
-    for i in range(len(x)):
-        v = Vector(x[i],y[i],z[i])
-        x1[i],y1[i],z1[i] = q.rotate(v)
-        roll, pitch, yaw = q.to_euler()
-        x2[i],y2[i],z2[i] = Rotation_Euler(v,roll=roll, pitch=pitch, yaw=yaw)
+    v = Vector3D(np.stack((x,y,z),axis=-1))
 
-    #fig = plt.figure()
-    fig, (ax1,ax2) = plt.subplots(1, 2, figsize=(12, 6), subplot_kw={'projection': '3d'})
-    #ax = fig.add_subplot(111, projection='3d')
+    v1 = q.rotate(v)
+    x1,y1,z1 = v1[...,0],v1[...,1],v1[...,2]
+
+    roll, pitch, yaw = q.to_euler()
+    v2 = Rotation_Euler(v,roll=roll, pitch=pitch, yaw=yaw)
+    x2,y2,z2 = v2[...,0],v2[...,1],v2[...,2]
+
+    fig, (ax1,ax2) = plt.subplots(1, 2, figsize=(18, 9), subplot_kw={'projection': '3d'})
+
     ax1.plot(x, y, z, label='original', color='blue')
     ax1.plot(x1, y1, z1, label='rotated1', color='green',alpha=0.5)
-    ax1.plot(x2, y2, z2, label='rotated2', color='red',alpha=0.5)
+    ax1.plot(x2, y2, z2, label='rotated2', color='red',alpha=0.1)
     ax1.set_xlim(-5, 5)
     ax1.set_ylim(-5, 5)
     ax1.set_zlim(-5, 5)
@@ -33,14 +167,13 @@ if __name__ == '__main__':
 
     
     x,y,z = create_3d_surface()
-    x1,y1,z1 = np.zeros_like(x),np.zeros_like(y),np.zeros_like(z)
-    x2,y2,z2= np.zeros_like(x),np.zeros_like(y),np.zeros_like(z)
-    for i in range(len(x)):
-        for j in range(len(x[i])):
-            v = Vector(x[i,j],y[i,j],z[i,j])
-            x1[i,j],y1[i,j],z1[i,j] = q.rotate(v)
-            roll, pitch, yaw = q.to_euler()
-            x2[i,j],y2[i,j],z2[i,j] = Rotation_Euler(v,roll=roll, pitch=pitch, yaw=yaw)
+    v = Vector3D(np.stack((x,y,z),axis=-1))
+    
+    v1 = q.rotate(v)
+    x1,y1,z1 = v1[...,0],v1[...,1],v1[...,2]
+    
+    roll, pitch, yaw = q.to_euler()
+    #x2[i,j],y2[i,j],z2[i,j] = Rotation_Euler(v,roll=roll, pitch=pitch, yaw=yaw)
 
     ax2.plot_surface(x, y, z, cmap='viridis', alpha=0.2)
     ax2.set_xlim(-2, 2)
@@ -48,7 +181,7 @@ if __name__ == '__main__':
     ax2.set_zlim(-2, 2)
 
     ax2.plot_surface(x1, y1, z1, cmap='winter', alpha=0.4)
-    ax2.plot_surface(x2, y2, z2, cmap='autumn', alpha=0.6)
+    #ax2.plot_surface(x2, y2, z2, cmap='autumn', alpha=0.6)
 
     plt.show()
-
+'''
--- utils.py
+++ utils.py
@@ -1,25 +1,32 @@
 import numpy as np
 import math
 
-class Vector(np.ndarray):
-    def __new__(cls, *args):
-        assert len(args) == 3, "Vector must have 3 components"
-        obj = np.asarray(args, dtype=float).view(cls)
-        return obj
+
   
 
 class Vector3D(np.ndarray):
     def __new__(cls, array:np.ndarray):
         assert array.shape[-1] == 3, "Vector must have 3 components"
-        assert array.dtype == float, "Vector must have float components"
+        #assert array.dtype == float, "Vector must have float components"
         obj = array.view(cls)
         return obj
+    def _normalize(self):
+        norm = np.linalg.norm(self, axis=-1, keepdims=True)
+        if not np.allclose(norm, 0):
+            self /= norm
+        else:
+            raise ValueError("at least one vector has zero norm")
 
 class Quaternion4D(np.ndarray):
     def __new__(cls, array:np.ndarray):
         assert array.shape[-1] == 4, "Quaternion must have 4 components"
         obj = array.view(cls)
         return obj
+    
+    @classmethod
+    def from_scaler_vector(cls, s:np.ndarray, v:Vector3D):
+        #assert s.shape == v.shape[:-1], "scaler and vector must have the same shape"
+        return cls(np.concatenate([s, v], axis=-1))
     
     def __eq__(self, other):
         return np.allclose(self, other)
@@ -35,15 +42,17 @@ class Quaternion4D(np.ndarray):
 
             w1, x1, y1, z1 = [self[...,i].view(np.ndarray) for i in range(4)]
             w2, x2, y2, z2 = [other[...,i].view(np.ndarray) for i in range(4)]
-            return Quaternion4D(np.array([w1*w2 - x1*x2 - y1*y2 - z1*z2,
+            return Quaternion4D(np.stack([w1*w2 - x1*x2 - y1*y2 - z1*z2,
                                           w1*x2 + x1*w2 + y1*z2 - z1*y2,
                                           w1*y2 - x1*z2 + y1*w2 + z1*x2,
-                                          w1*z2 + x1*y2 - y1*x2 + z1*w2]).reshape(self.shape))
+                                          w1*z2 + x1*y2 - y1*x2 + z1*w2], axis=-1))
         return NotImplemented
+    
     def conjugate(self):
         return Quaternion4D(np.array([self[...,0], -self[...,1], -self[...,2], -self[...,3]]).reshape(self.shape))
+    
     def rotate(self, vector:Vector3D):
-        p_vector = Quaternion4D(np.concatenate([np.zeros_like(self[...,0:1]), vector], axis=-1))
+        p_vector = Quaternion4D(np.concatenate([np.zeros_like(vector[...,0:1]), vector], axis=-1))
         return (self * p_vector * self.conjugate())[...,1:]
     def to_matrix(self):
         w, x, y, z = [self[...,i].view(np.ndarray) for i in range(4)]
@@ -66,87 +75,22 @@ class Quaternion4D(np.ndarray):
         cosy_cosp = 1 - 2 * (y**2 + z**2)
         yaw = np.arctan2(siny_cosp, cosy_cosp)
         
-        return roll, pitch, yaw
+        return roll.squeeze(), pitch.squeeze(), yaw.squeeze()
         
-class Quaternion(np.ndarray):
-    def __new__(cls, *args):
-        assert len(args) == 4
-        obj = np.asarray(args, dtype=float).view(cls)
-        #obj._normalize()
-        return obj
-    
-    @classmethod
-    def from_vector(cls, vector):
-        return cls(0, *vector)
 
-    @classmethod
-    def from_scaler_vector(cls, s, v:Vector):
-        return cls(s, *v)
-    
-    def __eq__(self, other):
-        return np.allclose(self, other)
-    
-    def _normalize(self):
-        norm = np.linalg.norm(self)
-        if norm != 0:
-            self /= norm
-    
-    def __mul__(self, other):
-        if isinstance(other, Quaternion):
-            w1, x1, y1, z1 = self
-            w2, x2, y2, z2 = other
-            return Quaternion(w1*w2 - x1*x2 - y1*y2 - z1*z2,
-                              w1*x2 + x1*w2 + y1*z2 - z1*y2,
-                              w1*y2 - x1*z2 + y1*w2 + z1*x2,
-                              w1*z2 + x1*y2 - y1*x2 + z1*w2)
-        return NotImplemented
-    
-    def conjugate(self):
-        return Quaternion(self[0], -self[1], -self[2], -self[3])
-    
-    def rotate(self, vector:Vector):
-        p_vector = Quaternion.from_vector(vector)
-        return (self * p_vector * self.conjugate())[1:]
-    
-    def to_matrix(self):
-        w, x, y, z = self
-        return np.array([[1 - 2*y**2 - 2*z**2, 2*x*y - 2*z*w, 2*x*z + 2*y*w],
-                         [2*x*y + 2*z*w, 1 - 2*x**2 - 2*z**2, 2*y*z - 2*x*w],
-                         [2*x*z - 2*y*w, 2*y*z + 2*x*w, 1 - 2*x**2 - 2*y**2]])
-    
-    def to_euler(self):
-        w, x, y, z = self
-        # Roll (rotation about x-axis)
-        sinr_cosp = 2 * (w * x + y * z)
-        cosr_cosp = 1 - 2 * (x**2 + y**2)
-        roll = math.atan2(sinr_cosp, cosr_cosp)
-        
-        # Pitch (rotation about y-axis)
-        sinp = 2 * (w * y - z * x)
-        if abs(sinp) >= 1:
-            pitch = math.copysign(math.pi / 2, sinp)  # Use 90 degrees if out of range
-        else:
-            pitch = math.asin(sinp)
-        
-        # Yaw (rotation about z-axis)
-        siny_cosp = 2 * (w * z + x * y)
-        cosy_cosp = 1 - 2 * (y**2 + z**2)
-        yaw = math.atan2(siny_cosp, cosy_cosp)
-        
-        return roll, pitch, yaw
-    
 
-def Rotation_Euler(vector:Vector,roll, pitch, yaw):
+def Rotation_Euler(vector:Vector3D,roll, pitch, yaw):
     # Convert the Euler angles to a rotation matrix
     R = np.array([[np.cos(yaw)*np.cos(pitch), np.cos(yaw)*np.sin(pitch)*np.sin(roll) - np.sin(yaw)*np.cos(roll), np.cos(yaw)*np.sin(pitch)*np.cos(roll) + np.sin(yaw)*np.sin(roll)],
                   [np.sin(yaw)*np.cos(pitch), np.sin(yaw)*np.sin(pitch)*np.sin(roll) + np.cos(yaw)*np.cos(roll), np.sin(yaw)*np.sin(pitch)*np.cos(roll) - np.cos(yaw)*np.sin(roll)],
                   [-np.sin(pitch), np.cos(pitch)*np.sin(roll), np.cos(pitch)*np.cos(roll)]])
     # Rotate the vector by the rotation matrix
-    return np.dot(R, vector)
+    #return np.dot(R, vector)
+    return np.dot(vector,R.T)
 
 def create_3d_curve():
     # Create a 3D curve
-    t = np.linspace(-2*np.pi, 2*np.pi, 100)
+    t = np.linspace(-2*np.pi, 2*np.pi, 900)
     x = 0.3*np.sin(10*t)
     y = 0.3*np.cos(10*t)
     z = t 
